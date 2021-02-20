846c0bdd6d316db0eb8ba0423bed0eb9510e15a4
Jaime Tasca Junior <jaime.tasca.jr@gmail.com>
2021-02-20 13:29:02 -0300

    adding the edit parameters and validations

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import '../providers/product.dart';
 
 class EditProductScreen extends StatefulWidget {
   static const routeName = 'edit-product';
@@ -10,13 +11,46 @@ class EditProductScreen extends StatefulWidget {
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocus = FocusNode();
+  final _imageUrlFocus = FocusNode();
+  final _imageUrlController = TextEditingController();
+  final _form = GlobalKey<FormState>();
+  var _editProduct = Product(
+    id: null,
+    title: '',
+    price: 0,
+    description: '',
+    imageUrl: '',
+  );
+
+  @override
+  void initState() {
+    _imageUrlFocus.addListener(_updateImageUrl);
+    super.initState();
+  }
 
   // When use focus nodes, make sure to always dispose of it on the dispose method above.
   @override
   void dispose() {
+    _imageUrlFocus.removeListener(_updateImageUrl);
     _priceFocusNode.dispose();
     _descriptionFocus.dispose();
+    _imageUrlFocus.dispose();
+    _imageUrlController.dispose();
     super.dispose();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageUrlFocus.hasFocus) {
+      setState(() {});
+    }
+  }
+
+  void _saveForm() {
+    final isValid = _form.currentState.validate();
+    if (!isValid) {
+      return;
+    }
+    _form.currentState.save();
   }
 
   @override
@@ -24,17 +58,43 @@ class _EditProductScreenState extends State<EditProductScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Edit Product'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: _saveForm,
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Form(
+          // It's a global key usually use in forms, because it interacts the "global" aspects of the code.
+          // Since it's a statefull widget it can access the values even it has changes.
+          key: _form,
           child: ListView(
             children: [
               TextFormField(
-                decoration: InputDecoration(labelText: 'Title'),
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                ),
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please provide the Title';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _editProduct = Product(
+                    title: value,
+                    price: _editProduct.price,
+                    description: _editProduct.description,
+                    imageUrl: _editProduct.imageUrl,
+                    id: null,
+                  );
                 },
               ),
               TextFormField(
@@ -45,13 +105,111 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_descriptionFocus);
                 },
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please provide a Price';
+                  }
+                  if (double.tryParse(value) == null) {
+                    return 'Price should be a valid number';
+                  }
+                  if (double.parse(value) <= 0) {
+                    return 'Price should be higher than 0.';
+                  }
+
+                  return null;
+                },
+                onSaved: (value) {
+                  _editProduct = Product(
+                    title: _editProduct.title,
+                    price: double.parse(value),
+                    description: _editProduct.description,
+                    imageUrl: _editProduct.imageUrl,
+                    id: null,
+                  );
+                },
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Description'),
                 maxLines: 3,
                 keyboardType: TextInputType.multiline,
                 focusNode: _descriptionFocus,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please provide a Description';
+                  }
+
+                  return null;
+                },
+                onSaved: (value) {
+                  _editProduct = Product(
+                    title: _editProduct.title,
+                    price: _editProduct.price,
+                    description: value,
+                    imageUrl: _editProduct.imageUrl,
+                    id: null,
+                  );
+                },
               ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.only(top: 8, right: 10),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.grey),
+                    ),
+                    child: _imageUrlController.text.isEmpty
+                        ? Text('Enter a URL')
+                        : FittedBox(
+                            child: Image.network(
+                              _imageUrlController.text,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Image URL'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      controller: _imageUrlController,
+                      focusNode: _imageUrlFocus,
+                      onEditingComplete: () {
+                        setState(() {});
+                      },
+                      onFieldSubmitted: (_) {
+                        _saveForm();
+                      },
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please provide a Image URL.';
+                        }
+
+                        if (!value.startsWith('http') && !value.startsWith('https')) {
+                          return 'Please provide a valid URL.';
+                        }
+
+                        if (!value.endsWith('.png') && !value.endsWith('.jpg') && !value.endsWith('.jpeg')) {
+                          return 'Please provide a valid image URL';
+                        }
+
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _editProduct = Product(
+                          title: _editProduct.title,
+                          price: _editProduct.price,
+                          description: _editProduct.description,
+                          imageUrl: value,
+                          id: null,
+                        );
+                      },
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
